c4b4d7fb17e62db6b5682245f3e917e8e7b5b04f
Jovi Du <ijovi23@gmail.com>
2016-04-13 00:53:19 +0800

    series

--- JvAsync/JvAsync.h
+++ JvAsync/JvAsync.h
@@ -9,11 +9,14 @@
 #import <Foundation/Foundation.h>
 
 typedef void(^JvAsyncCallback)(NSError *error, id data);
+typedef void(^JvFunc)(JvAsyncCallback callback);
 typedef void(^JvWaterfallFunc) (id data, JvAsyncCallback callback);
 
 @interface JvAsync : NSObject
 
 + (instancetype)async;
+
+- (void)seriesTasks:(NSArray<JvFunc> *)tasks callback:(JvAsyncCallback)callback;
 
 - (void)waterfallTasks:(NSArray<JvWaterfallFunc> *)tasks callback:(JvAsyncCallback)callback;
 
--- JvAsync/JvAsync.m
+++ JvAsync/JvAsync.m
@@ -10,19 +10,27 @@
 
 @interface JvAsync ()
 
-@property (strong, atomic) NSMutableArray<JvWaterfallFunc> *taskStack;
+@property (strong, nonatomic) NSMutableArray *taskStack;
 
 @property (copy, nonatomic) JvAsyncCallback callback;
 
 @property (strong, atomic) id currentData;
+@property (strong, nonatomic) NSMutableArray *results;
 
 @property (strong, atomic) id currentError;
 
-@property (copy, nonatomic) JvWaterfallFunc nextFunc;
 
 @end
 
 @implementation JvAsync
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.results = [NSMutableArray array];
+    }
+    return self;
+}
 
 + (instancetype)async {
     JvAsync *instance = [[JvAsync alloc]init];
@@ -33,45 +41,70 @@
     NSLog(@"----- JvAsync Dealloc -----");
 }
 
-- (void)waterfallTasks:(NSArray<JvWaterfallFunc> *)tasks callback:(JvAsyncCallback)callback {
-    
-    if (tasks.count <= 0) {
-        callback(nil, nil);
-        return;
-    }
-    
-    self.taskStack = [NSMutableArray array];
-    for (NSInteger i = tasks.count - 1; i >= 0; i--) {
-        [self.taskStack addObject:tasks[i]];
-    }
+#pragma mark - series
+
+- (void)seriesTasks:(NSArray<JvFunc> *)tasks callback:(JvAsyncCallback)callback {
     
     self.callback = callback;
     
-    [self doTask];
-}
-
-- (JvWaterfallFunc)nextFunc {
-    if (self.taskStack && self.taskStack.count) {
-        JvWaterfallFunc retFunc = self.taskStack.lastObject;
-        [self.taskStack removeLastObject];
-        return retFunc;
+    if ([self setTaskStackWithTasks:tasks]) {
+        [self series_doTask];
+    }else{
+        [self doCallback];
     }
     
-    return NULL;
 }
 
-- (void)doTask {
-    
-    JvWaterfallFunc func = [self nextFunc];
+- (void)series_doTask {
+    JvFunc func = (JvFunc)[self popTaskStack];
     
     if (func == NULL) {
-        if (self.callback) {
-            self.callback(self.currentError, self.currentData);
-        }
+        [self doCallback];
         return;
     }
     
-//    __weak typeof(self) ws = self;
+    //enter a child thread
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        func(^(NSError *error, id data){
+            
+            //return the main thread
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.currentError = error;
+                [self addToResultsWithData:data];
+                if (self.currentError) {
+                    [self doCallback];
+                }else{
+                    [self series_doTask];
+                }
+            });
+            
+        });
+    });
+}
+
+#pragma mark - waterfall
+
+- (void)waterfallTasks:(NSArray<JvWaterfallFunc> *)tasks callback:(JvAsyncCallback)callback {
+    
+    self.callback = callback;
+    
+    if ([self setTaskStackWithTasks:tasks]) {
+        [self waterfall_doTask];
+    }else{
+        [self doCallback];
+    }
+    
+}
+
+- (void)waterfall_doTask {
+    
+    JvWaterfallFunc func = (JvWaterfallFunc)[self popTaskStack];
+    
+    if (func == NULL) {
+        [self doCallback];
+        return;
+    }
     
     //enter a child thread
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
@@ -83,11 +116,9 @@
                 self.currentError = error;
                 self.currentData = data;
                 if (self.currentError) {
-                    if (self.callback) {
-                        self.callback(self.currentError, self.currentData);
-                    }
+                    [self doCallback];
                 }else{
-                    [self doTask];
+                    [self waterfall_doTask];
                 }
                 
             });
@@ -95,6 +126,55 @@
         });
     });
     
+}
+
+#pragma mark - parallel
+
+- (void)parallelTasks:(NSArray<JvFunc> *)tasks callback:(JvAsyncCallback)callback {
+    
+    self.callback = callback;
+    
+    
+    
+}
+
+#pragma mark - General
+
+- (BOOL)setTaskStackWithTasks:(NSArray *)tasks {
+    if ((!tasks) || tasks.count <= 0) {
+        return NO;
+    }
+    
+    self.taskStack = [NSMutableArray array];
+    for (NSInteger i = tasks.count - 1; i >= 0; i--) {
+        [self.taskStack addObject:tasks[i]];
+    }
+    
+    return YES;
+}
+
+- (void(^)())popTaskStack {
+    if (self.taskStack && self.taskStack.count) {
+        void(^retFunc)() = self.taskStack.lastObject;
+        [self.taskStack removeLastObject];
+        return retFunc;
+    }
+    
+    return NULL;
+}
+
+- (void)addToResultsWithData:(id)data {
+    if (data) {
+        [self.results addObject:data];
+    }else{
+        [self.results addObject:[NSNull null]];
+    }
+}
+
+- (void)doCallback {
+    if (self.callback) {
+        self.callback(self.currentError, self.currentData);
+    }
 }
 
 @end
